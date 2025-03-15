fcc78a8ed956df5f290583c2bbeb77015e5dfdcc
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-03-15 08:24:00 +0600

    update setting

--- TuitionVault/settings.py
+++ TuitionVault/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://tuitionvault.onrender.com','https://durjoykumar177.github.io','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://durjoykumar177.github.io','https://*.127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
